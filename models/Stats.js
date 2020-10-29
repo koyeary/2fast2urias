0b0d919204cc90e74319b51c913a2ae771f206a1
koyeary <katyeary@gmail.com>
2020-10-29 09:24:00 -0700

    stats model created to track mood and causation

---
+++ models/Stats.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StatsSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      },
+      mood: {
+        type: Number,
+        required: true
+      },
+      energy: {
+        type: Number,
+        required: true
+      }
+      hoursSlept: {
+        type: Number
+      },
+      interruptedSleep: {
+        type: Number
+      },
+      socializing: {
+        type: Number
+      },
+      creativity: {
+        type: Boolean
+      },
+      meditation: {
+        type: Boolean
+      },
+      sexualActivity: {
+        type: Boolean
+      },
+      menstruating: {
+        type: Boolean
+      },
+      meals: [
+        {
+          howMuch: {
+            type: Number
+          },
+          howHealthy: {
+            type: Number
+          }
+        }
+      ],
+      physicalActivity: [
+        { 
+          howMuch: {
+            type: Number
+          },
+          howStrenuous: {
+            type: Number
+          }
+        }
+      ],
+      stressOrAnxiety: {
+          type: Boolean
+      },
+      conflict: {
+          type: Boolean
+      },
+      Irritability: {
+        type: Number
+      },
+      Substances: [
+        { 
+          yesOrNo: {
+            type: Boolean
+          },
+          howStrenuous: {
+            type: Number
+          }
+        }
+      ],
+      Notes: {
+        type: String
+      }
+    });
+    
+    module.exports = mongoose.model('stats', StatsSchema);
+    
