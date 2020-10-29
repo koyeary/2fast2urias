7d8410ac478b0afb7ad8d9223bb53e1a9ce1317c
koyeary <katyeary@gmail.com>
2020-10-29 12:09:19 -0700

    set up components

---
+++ client/src/components/forms/RegisterForm.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+//import { connect } from 'react-redux';
+
+const RegisterForm = () => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default RegisterForm;
