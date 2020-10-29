7a3f9765bab51074ad22a8c2d735e23f2e7044ae
koyeary <katyeary@gmail.com>
2020-10-29 15:57:29 -0700

    create register form

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import Landing from './components/layout/Landing';
 import Navbar from './components/layout/NavBar';
 
 // Redux
-//import { Provider } from 'react-redux';
-//import store from './store';
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 const App = () => {
-  /* useEffect(() => {
+  useEffect(() => {
     // check for token in LS
     if (localStorage.token) {
       setAuthToken(localStorage.token);
@@ -17,21 +19,23 @@ const App = () => {
     store.dispatch(loadUser());
 
     // log user out from all tabs if they log out in one tab
-    window.addEventListener('storage', () => {
+/*     window.addEventListener('storage', () => {
       if (!localStorage.token) store.dispatch({ type: LOGOUT });
-    });
+    }); */
   }, []);
- */
+
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route component={Routes} />
-        </Switch>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,11 +1,17 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Landing = () => {
 
     return(
         <Fragment>
             <h1>Landing page</h1>
+            <p>
+                <Link className="btn btn-primary btn-lg" to="/register">Sign Up</Link>
+                <Link className="btn btn-primary btn-lg" to="/dashboard">Sign In</Link>
+            </p>
         </Fragment>
     )
 }
